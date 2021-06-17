d3a7a11f8f17ad42a18b38e3d33a6ab610b1295c
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2021-06-17 16:20:57 -0600

    added babel to express app

--- post-service/src/index.js
+++ post-service/src/index.js
@@ -1,17 +1,18 @@
-const express = require('express');
+import express,{json} from 'express';
 
 const port = 4000;
 const app = express();
 
 
-app.use(express.json());
-
+app.use(json());
 
 app.get('/posts', (req, res)=>{
-    res.status(200).send({});
+    res.status(200).send({
+        id:1,
+        name: "hola mundo"
+    });
 });
 
-
-app.listen(4000,()=>{
+app.listen(port,() => {
     console.log(`server on port  ${port}`)
 });
